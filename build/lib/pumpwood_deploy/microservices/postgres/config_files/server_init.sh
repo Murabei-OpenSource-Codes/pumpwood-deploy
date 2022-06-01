4e642714940d608e320bb3ec7575f8ccc60098a7
André Andrade Baceti <a.baceti@murabei.com>
2022-06-01 13:50:30 -0300

    Ajusting package deploy

--- build/lib/pumpwood_deploy/microservices/postgres/config_files/server_init.sh
+++
@@ -1,9 +0,0 @@
-cp /etc/secrets/ssl_key $PGDATA/server.key
-cp /etc/secrets/ssl_crt $PGDATA/server.crt
-
-chmod 600 $PGDATA/server.key
-chmod 600 $PGDATA/server.crt
-
-echo "ssl=on" >> $PGDATA/postgresql.conf
-echo "work_mem=500MB" >> $PGDATA/postgresql.conf
-echo "shared_buffers=1GB" >> $PGDATA/postgresql.conf
