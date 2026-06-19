abe10308129c4940713cdaa9fd8c23f356c26828
André Andrade Baceti <a.baceti@murabei.com>
2026-06-19 18:02:08 -0300

    Building a new version 2.0.5-b.0

--- src/pumpwood_deploy/kubernets/bash_templates/configmap_keyname.sh
+++ src/pumpwood_deploy/kubernets/bash_templates/configmap_keyname.sh
@@ -1,5 +1,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-kubectl delete configmap {name}
+kubectl delete configmap --ignore-not-found --namespace={namespace} {name}
 kubectl create configmap {name}\
     --from-file="{keyname}=$SCRIPTPATH/{file_name}"\
     --namespace={namespace}
